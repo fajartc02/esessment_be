48812bee419c75e75b568e9a2115d6ab600f75e8
fajartc02 <fajartc02@gmail.com>
2024-02-18 21:52:21 +0700

    changes id to uuid when response login

--- controllers/auth/login.controllers.js
+++ controllers/auth/login.controllers.js
@@ -1,20 +1,23 @@
-const table = 'tb_m_users'
 const { queryGET } = require('../../helpers/query')
 const security = require('../../helpers/security')
 const auth = require('../../helpers/auth')
 const response = require('../../helpers/response')
+const table = require('../../config/table')
+const idToUuid = require('../../helpers/idToUuid')
 
 module.exports = {
     login: async(req, res) => {
         console.log(req.body);
         try {
-            await queryGET(table, `WHERE noreg = '${req.body.noreg}' AND is_activated = true`)
+            await queryGET(table.tb_m_users, `WHERE noreg = '${req.body.noreg}' AND is_activated = true`)
                 .then(async(result) => {
                     if (result.length > 0) {
                         let hashPassword = result[0].password
                         await security.decryptPassword(req.body.password, hashPassword).then(async decryptPass => {
-                            console.log(decryptPass);
+                            // console.log(decryptPass);
                             if (decryptPass) {
+                                result[0].line_id = await idToUuid(table.tb_m_lines, 'line_id', result[0].line_id)
+                                result[0].group_id = await idToUuid(table.tb_m_groups, 'group_id', result[0].group_id)
                                 let token = await auth.generateToken({ name: result[0].name, noreg: result[0].noreg })
                                 response.success(res, 'Success to Login', { data: result[0], token })
                             }
--- helpers/idToUuid.js
+++ helpers/idToUuid.js
@@ -4,6 +4,7 @@ const { database } = require('../config/database')
 const idToUuid = async(table, col, id) => {
     console.log(`SELECT uuid as ${col} FROM ${table} WHERE ${col} = '${id}'`);
     let rawId = await database.query(`SELECT uuid as ${col} FROM ${table} WHERE ${col} = '${id}'`)
+    console.log(rawId);
     return rawId.rows[0][col]
 }
 
