a11a8980bbeb6f58fac3af41b3e9a18f474387ad
Khesa <vandikalvandi@gmail.com>
2024-03-18 00:37:30 +0700

    add pagination algorithm

--- controllers/master/zones.controllers.js
+++ controllers/master/zones.controllers.js
@@ -10,25 +10,83 @@ const moment = require("moment")
 const { uuid } = require("uuidv4")
 
 module.exports = {
+  /**
+   * @param {*} req
+   * @param {*} res 
+   * @param {JSON} req.query.id is determine for detail usecase
+   */
   getZones: async (req, res) => {
     try
     {
-      const zones = await queryCustom(`
-        select
-            tml.uuid as line_id,
-                tmz.uuid as zone_id,
-                tmz.zone_nm,
-                tml.line_nm,
-                tmz.created_by,
-                tmz.created_dt
-        from
-            tb_m_zones tmz
-            join tb_m_lines tml on tmz.line_id = tml.line_id
-        where
-          tmz.deleted_dt is null
-      `)
+      let { id, line_id, limit, current_page, zone_nm } = req.query
+      const fromCondition = ` ${table.tb_m_zones} tmz join ${table.tb_m_lines} tml on tmz.line_id = tml.line_id `
 
-      response.success(res, "Success to get zones", zones.rows)
+      current_page = parseInt(current_page ?? 1)
+      limit = parseInt(limit ?? 10)
+
+      let filterCondition = [
+        ' and tmz.deleted_dt is null '
+      ]
+
+      let zoneQuery = `
+          select
+              tml.uuid as line_id,
+              tmz.uuid as zone_id,
+              tmz.zone_nm,
+              tml.line_nm,
+              tmz.created_by,
+              tmz.created_dt
+          from
+              ${fromCondition}
+          where
+            1 = 1
+        `
+      //#region filter
+      if (id)
+      {
+        filterCondition.push(` tmz.uuid = '${id}' `)
+      }
+      if (line_id)
+      {
+        filterCondition.push(` tml.uuid = '${line_id}' `)
+      }
+      if (zone_nm)
+      {
+        filterCondition.push(` tml.zone_nm = '${zone_nm}' `)
+      }
+
+      const qOffset = (limit != -1 && limit) && current_page > 1 ? `OFFSET ${limit * (current_page - 1)}` : ``
+      const qLimit = (limit != -1 && limit) ? `LIMIT ${limit}` : ``
+
+      filterCondition = filterCondition.join(' and ')
+      zoneQuery = zoneQuery.concat(` ${filterCondition} `)
+      zoneQuery = zoneQuery.concat(` order by tmz.created_by ${qLimit} ${qOffset} `)
+      //#endregion
+
+      const zones = await queryCustom(zoneQuery)
+      const nullId = id == null || id == -1 || id == ''
+      let result = zones.rows
+
+      if (zones.rows.length > 0)
+      {
+        if (nullId)
+        {
+          const count = await queryCustom(`select count(tmz.zone_id) as count from ${fromCondition} where 1 = 1 ${filterCondition}`)
+          const countRows = count.rows[0]
+          result = {
+            current_page: current_page,
+            total_page: +countRows.count > 0 ? Math.ceil(countRows.count / +limit) : 0,
+            limit: limit,
+            list: zones.rows,
+          }
+        } 
+        else
+        {
+          result = result[0]
+        }
+      }
+
+      response.success(res, "Success to get zones", result)
     } catch (error)
     {
       console.log(error)
