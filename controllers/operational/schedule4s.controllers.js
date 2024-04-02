6989108426a34a219311207dd0e18da3ac7f7b44
Khesa <vandikalvandi@gmail.com>
2024-04-02 18:50:36 +0700

    fix where clause

--- controllers/operational/schedule4s.controllers.js
+++ controllers/operational/schedule4s.controllers.js
@@ -662,7 +662,10 @@ module.exports = {
       )
 
       itemCheckKanbans.rows = await Promise.all(itemCheckKanbans.rows.map(async (item) => {
-        const findings = await queryGET(table.v_4s_finding_list, `where deleted_dt is null`)
+        const findings = await queryGET(
+          table.v_4s_finding_list,
+          `where deleted_dt is null and schedule_item_check_kanban_id = ${item.schedule_item_check_kanban_id}`
+        )
         if (findings.length > 0)
         {
           item.findings = findings
