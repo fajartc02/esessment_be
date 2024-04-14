451f6d19329344905ee01fab5f11e4ca5712b25a
Khesa <vandikalvandi@gmail.com>
2024-04-14 14:16:09 +0700

    fix graph 4s

--- routes/v1/master/omItemCheckKanbans.js
+++ routes/v1/master/omItemCheckKanbans.js
@@ -1,17 +1,19 @@
 const router = require("express")()
 const {
-    getOemItemCheckKanbans,
-    postOemItemCheck,
-    editOemItemCheck,
-    deleteOemItemCheck
+    getOmItemCheckKanbans,
+    getOmGroupMachinesPaginate,
+    postOmItemCheck,
+    editOmItemCheck,
+    deleteOmItemCheck
 } = require("../../../controllers/master/itemCheckKanbanOm.controllers")
 
 const auth = require("../../../helpers/auth")
 
-router.put("/edit/:id", auth.verifyToken, editOemItemCheck)
-router.delete("/delete/:id", auth.verifyToken, deleteOemItemCheck)
-router.post("/add", auth.verifyToken, postOemItemCheck)
-router.get("/get", auth.verifyToken, getOemItemCheckKanbans)
+router.put("/edit/:id", auth.verifyToken, editOmItemCheck)
+router.delete("/delete/:id", auth.verifyToken, deleteOmItemCheck)
+router.post("/add", auth.verifyToken, postOmItemCheck)
+router.get("/get", auth.verifyToken, getOmItemCheckKanbans)
+router.get("/get/groups", auth.verifyToken, getOmGroupMachinesPaginate)
 
 
 module.exports = router
