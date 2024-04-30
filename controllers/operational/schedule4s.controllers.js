341615f04bf73a61f182cf3b879aa5ea50c9ab54
fajartc02 <fajartc02@gmail.com>
2024-04-30 13:42:55 +0700

    add methods, control_point 4S

--- controllers/operational/schedule4s.controllers.js
+++ controllers/operational/schedule4s.controllers.js
@@ -741,6 +741,8 @@ module.exports = {
               trsic.uuid as schedule_item_check_kanban_id,
               tmk.kanban_no,
               tmic.item_check_nm,
+              tmic.method,
+              tmic.control_point,
               tmic.standart_time::REAL as standart_time,
               trsic.actual_time::REAL actual_time,
               trsic.checked_date,
