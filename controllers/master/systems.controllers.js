2002f9127b01bce4394879343cb913fb85fa3c00
fajartc02 <fajartc02@gmail.com>
2024-05-05 21:50:57 +0700

    order by system desc

--- controllers/master/systems.controllers.js
+++ controllers/master/systems.controllers.js
@@ -10,29 +10,24 @@ const moment = require("moment")
 const { uuid } = require("uuidv4")
 
 module.exports = {
-    getSystems: async (req, res) => {
-        try
-        {
+    getSystems: async(req, res) => {
+        try {
             let systemType = req.params.system_type
-            if (!systemType)
-            {
+            if (!systemType) {
                 systemType = req.query.system_type
             }
-            if (systemType == '4S_OPT_DEPT')
-            {
+            if (systemType == '4S_OPT_DEPT') {
                 systemType = 'OPT_DEPT'
             }
 
             let whereType = ''
-            if (systemType)
-            {
+            if (systemType) {
                 whereType = `and system_type = '${systemType}'`
             }
 
             const freqs = await queryGET(
                 table.tb_m_system,
-                `where deleted_dt is null ${whereType} order by system_value`,
-                [
+                `where deleted_dt is null ${whereType} order by system_desc`, [
                     `row_number () over (
                             order by
                             created_dt
@@ -47,15 +42,13 @@ module.exports = {
             )
 
             response.success(res, 'Success to get system', freqs)
-        } catch (error)
-        {
+        } catch (error) {
             console.log(error);
             response.failed(res, 'Error to get system')
         }
     },
-    postSystem: async (req, res) => {
-        try
-        {
+    postSystem: async(req, res) => {
+        try {
             const insertBody = {
                 ...req.body,
                 uuid: uuid(),
@@ -64,15 +57,13 @@ module.exports = {
             const attrsInsert = await attrsUserInsertData(req, insertBody)
             const result = await queryPOST(table.tb_m_system, attrsInsert)
             response.success(res, "Success to add system", result.rows)
-        } catch (error)
-        {
+        } catch (error) {
             console.log(error)
             response.failed(res, error)
         }
     },
-    editSystem: async (req, res) => {
-        try
-        {
+    editSystem: async(req, res) => {
+        try {
             const updateBody = {
                 ...req.body,
             }
@@ -85,15 +76,13 @@ module.exports = {
             )
 
             response.success(res, "Success to edit system", result.rows)
-        } catch (error)
-        {
+        } catch (error) {
             console.log(error)
             response.failed(res, error)
         }
     },
-    deleteSystem: async (req, res) => {
-        try
-        {
+    deleteSystem: async(req, res) => {
+        try {
             let obj = {
                 deleted_dt: moment().format().split("+")[0].split("T").join(" "),
                 deleted_by: req.user.fullname,
@@ -106,8 +95,7 @@ module.exports = {
                 `WHERE uuid = '${req.params.id}'`
             )
             response.success(res, "Success to soft delete system", result.rows)
-        } catch (error)
-        {
+        } catch (error) {
             console.log(error)
             response.failed(res, error)
         }
