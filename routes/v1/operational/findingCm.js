90e6c0cc8f72ef6a687d2e04a4b91e7932f3ce91
fajartc02 <fajartc02@gmail.com>
2024-05-05 21:22:55 +0700

    enhance STW check

--- routes/v1/operational/findingCm.js
+++ routes/v1/operational/findingCm.js
@@ -1,22 +1,31 @@
-const router = require('express')()
+const router = require("express")();
 const {
-    uploadPinksheet,
-    getFindingCm,
-    uploadImageFinding,
-    signFinding,
-    editFindingCm,
-    deleteFinding
-} = require('../../../controllers/operational/findingCm.controllers')
+  uploadPinksheet,
+  getFindingCm,
+  uploadImageFinding,
+  signFinding,
+  editFindingCm,
+  deleteFinding,
+} = require("../../../controllers/operational/findingCm.controllers");
 
-const auth = require('../../../helpers/auth')
-const upload = require('../../../helpers/upload')
+const auth = require("../../../helpers/auth");
+const upload = require("../../../helpers/upload");
 
-router.get('/', auth.verifyToken, getFindingCm)
-router.put('/upload-sign', auth.verifyToken, signFinding);
-router.post('/upload-image', auth.verifyToken, upload.single('attachment'), uploadImageFinding);
-router.post('/upload', auth.verifyToken, upload.single('attachment'), uploadPinksheet);
-router.put('/edit/:id', auth.verifyToken, editFindingCm)
-router.delete('/delete/:id', auth.verifyToken, deleteFinding)
+router.get("/", auth.verifyToken, getFindingCm);
+router.put("/upload-sign", auth.verifyToken, signFinding);
+router.post(
+  "/upload-image",
+  auth.verifyToken,
+  upload.single("attachment"),
+  uploadImageFinding
+);
+router.post(
+  "/upload",
+  auth.verifyToken,
+  upload.single("attachment"),
+  uploadPinksheet
+);
+router.put("/edit/:id", auth.verifyToken, editFindingCm);
+router.delete("/delete/:id", auth.verifyToken, deleteFinding);
 
-
-module.exports = router
+module.exports = router;
--- routes/v1/operational/observations.js
+++ routes/v1/operational/observations.js
@@ -1,30 +1,47 @@
-const router = require('express')()
+const router = require("express")();
 const {
-    addScheduleObservation,
-    getScheduleObservations,
-    getSummaryObservations,
-    getDetailObservation,
-    addCheckObservation,
-    getObservationScheduleList,
-    deleteScheduleObservation,
-    getTodaySchedule
-} = require('../../../controllers/operational/observations.controllers')
-const auth = require('../../../helpers/auth')
-const upload = require('../../../helpers/upload')
+  editObservation,
+  addObservationCheck,
+  addFindingObsCheck,
+} = require("../../../controllers/operational/newObservation.controllers");
+const {
+  addScheduleObservation,
+  getScheduleObservations,
+  getSummaryObservations,
+  getDetailObservation,
+  addCheckObservation,
+  getObservationScheduleList,
+  deleteScheduleObservation,
+  getTodaySchedule,
+} = require("../../../controllers/operational/observations.controllers");
+const auth = require("../../../helpers/auth");
+const upload = require("../../../helpers/upload");
 
-router.get('/summary', auth.verifyToken, getSummaryObservations)
-router.get('/schedule', auth.verifyToken, getScheduleObservations)
+router.get("/summary", auth.verifyToken, getSummaryObservations);
+router.get("/schedule", auth.verifyToken, getScheduleObservations);
 
-router.get('/schedule/today', auth.verifyToken, getTodaySchedule)
+router.get("/schedule/today", auth.verifyToken, getTodaySchedule);
 
-router.get('/schedule/list', auth.verifyToken, getObservationScheduleList)
-router.delete('/schedule/list/delete/:id', auth.verifyToken, deleteScheduleObservation)
+router.get("/schedule/list", auth.verifyToken, getObservationScheduleList);
+router.delete(
+  "/schedule/list/delete/:id",
+  auth.verifyToken,
+  deleteScheduleObservation
+);
 
-router.get('/schedule/:id', auth.verifyToken, getDetailObservation)
-router.post('/schedule', auth.verifyToken, addScheduleObservation)
+router.get("/schedule/:id", auth.verifyToken, getDetailObservation);
+router.post("/schedule", auth.verifyToken, addScheduleObservation);
+
+router.post("/single-check-obs", auth.verifyToken, editObservation);
+router.post("/single-check-category", auth.verifyToken, addObservationCheck);
+router.post("/single-check-finding", auth.verifyToken, addFindingObsCheck);
 
 // Upload.single didn't used, just only for handle multipart/form-data
-router.post('/check', auth.verifyToken, upload.single('attachment'), addCheckObservation)
+router.post(
+  "/check",
+  auth.verifyToken,
+  upload.single("attachment"),
+  addCheckObservation
+);
 
-
-module.exports = router
+module.exports = router;
