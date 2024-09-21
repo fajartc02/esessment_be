4ae50945fc855169cd68b2fabfa7b167350a44ed
fajartc02 <fajartc02@gmail.com>
2024-09-21 21:01:38 +0700

    [FAJAR-0003] add edit feature for schedule stw observation

--- helpers/getLastIdData.js
+++ helpers/getLastIdData.js
@@ -1,16 +1,17 @@
 const { queryGET } = require("./query");
 
 async function getLastIdData(table, columnId) {
-    return await queryGET(table, ` ORDER BY ${columnId} DESC LIMIT 1`)
-        .then((result) => {
-            console.log(result);
-            if (result.length === 0) return 0
-            return result[0][columnId]
-        }).catch((err) => {
-            console.log(err);
-            return err
-        });
-
+  //   return `(SELECT COALESCE(MAX(${columnId}), 0) AS ${columnId} FROM ${table})`;
+  return await queryGET(table, ` ORDER BY ${columnId} DESC LIMIT 1`)
+    .then((result) => {
+      console.log(result);
+      if (result.length === 0) return 0;
+      return result[0][columnId];
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
 }
 
-module.exports = getLastIdData
+module.exports = getLastIdData;
