490166320a1b69e903b1e628a29a6891275bf1d9
zaenal-blip <arif4321456789@gmail.com>
2026-06-08 11:45:10 +0700

    fix: fixing select line and shift

--- routes/v1/master/lines.js
+++ routes/v1/master/lines.js
@@ -66,6 +66,6 @@ const auth = require('../../../helpers/auth')
 router.put('/edit/:id', auth.verifyToken, editLine)
 router.delete('/delete/:id', auth.verifyToken, deleteLine)
 router.post('/', auth.verifyToken, postLine)
-router.get('/', auth.verifyToken, getLinesOpts)
+router.get('/', getLinesOpts)
 
 module.exports = router
