3d9a7334587428e6a23bd7b1ea5072e853aa21f2
zaenal-blip <arif4321456789@gmail.com>
2026-07-17 11:45:02 +0700

    fix: fix security issue blind sql injection in login

--- controllers/auth/login.controllers.js
+++ controllers/auth/login.controllers.js
@@ -1,54 +1,55 @@
-const { queryGET } = require("../../helpers/query");
 const security = require("../../helpers/security");
 const auth = require("../../helpers/auth");
 const response = require("../../helpers/response");
 const table = require("../../config/table");
 const idToUuid = require("../../helpers/idToUuid");
+const { database } = require("../../config/database");
 
 module.exports = {
   login: async (req, res) => {
-    console.log(req.body);
     try {
-      await queryGET(
-        table.tb_m_users,
-        `WHERE noreg = '${req.body.noreg}' AND is_activated = true AND deleted_dt IS NULL`
-      ).then(async (result) => {
-        if (result.length > 0) {
-          let hashPassword = result[0].password;
-          await security
-            .decryptPassword(req.body.password, hashPassword)
-            .then(async (decryptPass) => {
-              // console.log(decryptPass);
-              if (decryptPass) {
-                result[0].line_id = await idToUuid(
-                  table.tb_m_lines,
-                  "line_id",
-                  result[0].line_id
-                );
-                result[0].group_id = await idToUuid(
-                  table.tb_m_groups,
-                  "group_id",
-                  result[0].group_id
-                );
-                // console.log(result[0]);
-                let token = await auth.generateToken({
-                  name: result[0].name,
-                  noreg: result[0].noreg,
-                });
-                response.success(res, "Success to Login", {
-                  data: result[0],
-                  token,
-                });
-              }
-            });
+      // Parameterized query: $1 prevents SQL injection on noreg input
+      const queryResult = await database.query(
+        `SELECT * FROM ${table.tb_m_users} WHERE noreg = $1 AND is_activated = true AND deleted_dt IS NULL`,
+        [req.body.noreg]
+      );
+
+      const result = queryResult.rows;
+
+      if (result.length > 0) {
+        let hashPassword = result[0].password;
+        const decryptPass = await security.decryptPassword(req.body.password, hashPassword);
+
+        if (decryptPass) {
+          result[0].line_id = await idToUuid(
+            table.tb_m_lines,
+            "line_id",
+            result[0].line_id
+          );
+          result[0].group_id = await idToUuid(
+            table.tb_m_groups,
+            "group_id",
+            result[0].group_id
+          );
+
+          let token = await auth.generateToken({
+            name: result[0].name,
+            noreg: result[0].noreg,
+          });
+
+          response.success(res, "Success to Login", {
+            data: result[0],
+            token,
+          });
         } else {
-          // User not found in DB
-          throw null;
+          // Password salah
+          throw false;
         }
-      });
+      } else {
+        // User not found in DB
+        throw null;
+      }
     } catch (error) {
-      // let msg = error
-      // if (!error) msg = 'User belum di aktivasi, silahkan kontak admin'
       console.log(error);
       response.notAllowed(
         res,
